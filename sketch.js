e45603c42176f96ab76487fe772862596f5a22fc
vidhi123654 <82191902+vidhi123654@users.noreply.github.com>
2021-06-22 18:23:15 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -62,7 +62,7 @@ function draw() {
 
 function keyPressed(){
 	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:50,y:-45})
+		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:50,y:-45})
 	}
 }
 
